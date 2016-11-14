73523ca67216356cf71bc722e078efdc7cb30636
Andrew Pan <andrew.pan@students.olin.edu>
2016-11-14 17:51:24 -0500

    working version of first pass communication model

--- code/communication.py
+++ code/communication.py
@@ -8,6 +8,8 @@ from mesa.space import MultiGrid
 COOPERATE = -1
 DEFECT = -2
 NO_ACTION = -3
+payout = {(COOPERATE,COOPERATE):(3,3), (COOPERATE,DEFECT):(0,5), (COOPERATE,NO_ACTION):(2,-5), (COOPERATE,NO_ACTION):(2,-5), (DEFECT,COOPERATE):(5,0), (DEFECT,DEFECT):(1,1), (DEFECT,NO_ACTION):(2,-5), (NO_ACTION,NO_ACTION):(-5,-5), (NO_ACTION,COOPERATE):(-5,2), (NO_ACTION,DEFECT):(-5,2)}
+
     
 class CommunicationAgent(Agent):
     
@@ -18,7 +20,7 @@ class CommunicationAgent(Agent):
         self.fsm_size = fsm_size
         self.num_tokens = num_tokens
         self.scores = []
-        self.decision = None
+        self.decision = NO_ACTION
 
         self.gen_automata(fsm_size, num_tokens)
         
@@ -28,7 +30,7 @@ class CommunicationAgent(Agent):
         for state in range(fsm_size):
             flip = random.random()
             if (flip < 0.5):
-                self.action_map[state] = random.randint(1, num_tokens)
+                self.action_map[state] = random.randint(1, num_tokens-1)
             else: 
                 self.action_map[state] = random.choice([COOPERATE, DEFECT])
 
@@ -37,12 +39,14 @@ class CommunicationAgent(Agent):
         for pair in self.transition_table.keys():
             self.transition_table[pair] = random.randrange(fsm_size)
             
+        print ('ACTION')
         print(self.action_map)
+        print ('TRANSITION')
         print(self.transition_table)
 
     def choose_action(self):
         # If decision is already made, return 0
-        if decision:
+        if (self.decision == COOPERATE) or (self.decision == DEFECT):
             return 0
         # Check to see if current state leads to a decision
         elif(self.action_map[self.state] == COOPERATE) or (self.action_map[self.state] == DEFECT):
@@ -58,29 +62,10 @@ class CommunicationAgent(Agent):
         if (self.decision != None):
             self.set_state(self.transition_table[(self.state, token)])
     
-    def reset_scores(self):
+    def reset(self):
+        self.state = 0
         self.scores = []
-
-    def choose_action(self):
-        # If decision is already made, return 0
-        if decision:
-            return 0
-        # Check to see if current state leads to a decision
-        else if (self.action_map[self.state] == COOPERATE) or (self.action_map[self.state] == DEFECT):
-            self.decision = self.action_map[self.state]
-            return 0
-        # Else, return token specified by current state
-        else:
-            return self.action_map[self.state]
-            
-    def handle_token(self, token):
-        #takes in receiving token and selects next state
-        # If a decision hasn't been made, we move to the next state
-        if (self.decision != None):
-            self.set_state(self.transition_table[(self.state, token)])
-    
-    def reset_scores(self):
-        self.scores = []
+        self.decision = NO_ACTION
 
     def get_state(self):
         return self.state
@@ -93,8 +78,6 @@ class CommunicationAgent(Agent):
 
 class CommunicationModel(Model):
     
-    payout = {(COOPERATE,COOPERATE):(3,3), (COOPERATE,DEFECT):(0,5), (COOPERATE,NO_ACTION):(2,-5), (COOPERATE,NO_ACTION):(2,-5), (DEFECT,COOPERATE):(5,0), (DEFECT,DEFECT):(1,1), (DEFECT,NO_ACTION):(2,-5), (NO_ACTION,NO_ACTION):(-5,-5), (NO_ACTION,COOPERATE):(-5,2), (NO_ACTION,DEFECT):(-5,2)}
-    
     def __init__(self, N, max_chat, fsm_size, num_tokens):      #fsm_size is number of states, num_tokens is complexity of language
         
         # TODO: Create agents
@@ -104,25 +87,19 @@ class CommunicationModel(Model):
         self.max_chat = max_chat
         self.fsm_size = fsm_size
         self.num_tokens = num_tokens
-        # self.schedule = RandomActivation(self)
         
         self.agents = []
         
         for i in range(self.num_agents):
             agent = CommunicationAgent(i, self, fsm_size, num_tokens)
-            # self.schedule.add(agent)
             self.agents.append(agent)
 
     def run_one_round(self, agent1, agent2):
         pass
  
     def step(self):
-        # self.schedule.step()
-        pass
-
-        #reset all agents' automata to state 1 and score to 0
-        #play all agents against each other
-        #agent keeps track of its score
+        self.reset_agents()
+        self.selection()
     
     def selection(self):
         #choose two agents to PLAY
@@ -140,40 +117,46 @@ class CommunicationModel(Model):
             agent2 = random.choice(self.agents)
             while(agent1 == agent2):
                 agent2 = random.choice(self.agents)
+                break
             better_agent = agent1 if sum(agent1.scores) > sum(agent2.scores) else agent2
             
             if random.random() < .5: # roll for whether or not to mutate
                 if random.random() < .5: # roll for mutation type (change action map vs transition table)
                     if random.random() < .5: # roll for how to set new action map value (same as in agent's gen_automata function)
-                        better_agent.action_map[random.randrange(self.fsm_size)] = random.randint(1, num_tokens)
+                        better_agent.action_map[random.randrange(self.fsm_size)] = random.randint(1, self.num_tokens - 1)
                     else: 
                         better_agent.action_map[random.randrange(self.fsm_size)] = random.choice([COOPERATE, DEFECT])
                 else:
-                    better_agent.transition_table[random.choice(better_agent.transition_table.keys())] = random.randrange(fsm_size)
+                    better_agent.transition_table[random.choice(list(better_agent.transition_table.keys()))] = random.randrange(self.fsm_size)
             new_population.append(better_agent)
         self.agents = new_population
 
-    def play(agent1, agent2):
+    def play(self, agent1, agent2):
         chat_count = 0
-        while chat_count < num_chat :
+        while chat_count < self.max_chat:
             #still talking
             agent1_token = agent1.choose_action()
             agent2_token = agent2.choose_action()
-            
+
             agent1.handle_token(agent2_token)
             agent2.handle_token(agent1_token)
-
+            
             chat_count += 1
-
+            
+        agent1_score, agent2_score = payout[(agent1.decision, agent2.decision)]
+        
         agent1.scores.append(agent1_score)
         agent2.scores.append(agent2_score)
+        print(agent1.scores)
+        print(agent2.scores)
         # return score of agents as a tuple (agent1_score, agent2_score)
     
     def reset_agents(self):
-        for agent in agents:
-            agent.set_state(0)
-            agent.reset_scores()
+        for agent in self.agents:
+            agent.reset()
 
 
 if __name__ == '__main__':
-    communicationModel = CommunicationModel(1, 3, 3, 3)
+    communicationModel = CommunicationModel(3, 3, 3, 3)
+    for i in range(50):
+        communicationModel.step()
