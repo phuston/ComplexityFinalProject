ee5d231b372b1b345ed63a8992e7396f22e6c510
Patrick Huston <patrickhuston@patricks-mbp.olin.edu>
2016-11-29 18:41:31 -0500

    Pushes correct version of communication script

--- code/communication.py
+++ code/communication.py
@@ -1,87 +1,139 @@
+"""communication.py: Implementation of the agent-based model described in Communication and Cooperation - Miller et. al"""
+
+__author__      = "Patrick Huston, Meg McCauley, Andrew Pan"
+
+
 import random, copy, itertools
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 from mesa import Agent, Model
-from mesa.time import RandomActivation
-from mesa.space import MultiGrid
-# from mesa.datacollection import DataCollector
+import logging
 
+logging.basicConfig(filename='output.log',level=logging.DEBUG)
+with open('output.log', 'w'):
+    pass
+
+# Final actions
 COOPERATE = -1
 DEFECT = -2
 NO_ACTION = -3
-payout = {(COOPERATE,COOPERATE):(5,5), (COOPERATE,DEFECT):(0,5), (COOPERATE,NO_ACTION):(2,-5), (COOPERATE,NO_ACTION):(2,-5), (DEFECT,COOPERATE):(5,0), (DEFECT,DEFECT):(1,1), (DEFECT,NO_ACTION):(2,-5), (NO_ACTION,NO_ACTION):(-5,-5), (NO_ACTION,COOPERATE):(-5,2), (NO_ACTION,DEFECT):(-5,2)}
 
-    
+# Payout mapping
+payout = {(COOPERATE,COOPERATE):(3,3), (COOPERATE,DEFECT):(0,5), (COOPERATE,NO_ACTION):(2,-5), (DEFECT,COOPERATE):(5,0), (DEFECT,DEFECT):(1,1), (DEFECT,NO_ACTION):(2,-5), (NO_ACTION,NO_ACTION):(-5,-5), (NO_ACTION,COOPERATE):(-5,2), (NO_ACTION,DEFECT):(-5,2)}
+
 class CommunicationAgent(Agent):
+    """ 
+    Represents an agent in the agent-based model investigated in 'Communication and Cooperation' - Miller, et. al
+
+    Holds an action map and transition table to define automata
+    """
     
     def __init__(self, unique_id, model, fsm_size, num_tokens):
         Agent.__init__(self, unique_id, model)
         self.state = 0
         self.unique_id = unique_id
-        self.fsm_size = fsm_size
-        self.num_tokens = num_tokens
         self.scores = []
         self.decision = NO_ACTION
 
         self.gen_automata(fsm_size, num_tokens)
         
     def gen_automata(self, fsm_size, num_tokens):
-        #map current state to action; 50% chance of choosing a token to send, 50% of choosing cooperate/defect
+        """ Generates action map and transition table to describe agent's automata
+
+        Creates an action map - map of state : action (token to send or final move)
+        Creates transition table - map of (state, token) : new state
+
+        Args:
+            fsm_size (int): Number of states in automata
+            num_tokens (int): Number of tokens allowed in communication
+        """
+
+        # Create action map
         self.action_map = {}
         for state in range(fsm_size):
             flip = random.random()
-            if (flip < 0.8):
+            if (flip < 0.5):
+                # .5 probability of choosing a token to send
                 self.action_map[state] = random.randint(1, num_tokens-1)
             else: 
+                # .5 probability of choosing cooperate/defect
                 self.action_map[state] = random.choice([COOPERATE, DEFECT])
 
-        #map from tuple (current state, received communication) to the next state
-        self.transition_table = dict.fromkeys([(state, received_token) for state in range(fsm_size) for received_token in range(num_tokens)])
-        for pair in self.transition_table.keys():
-            self.transition_table[pair] = random.randrange(fsm_size)
+        # Create transition table
+        self.transition_table = {}
+        states = range(fsm_size)
+        tokens = range(num_tokens)
+
+        state_token_pairs = itertools.product(states, tokens)
+        for state_token_pair in state_token_pairs:
+            self.transition_table[state_token_pair] = random.randrange(fsm_size)
 
     def choose_action(self):
-        # If decision is already made, return 0
-        if (self.decision == COOPERATE) or (self.decision == DEFECT):
+        """ Choose agent's action (token or decision) based on current state
+
+        If agent has already made a decision, send a 0 token
+        If agent's new state represents choosing an action, set decision and send 0 token
+        Else, send return communication token specified by current state
+        """
+
+        if (self.decision != NO_ACTION):
             return 0
-        # Check to see if current state leads to a decision
+
         elif(self.action_map[self.state] == COOPERATE) or (self.action_map[self.state] == DEFECT):
             self.decision = self.action_map[self.state]
             return 0
-        # Else, return token specified by current state
+
         else:
             return self.action_map[self.state]
             
     def handle_token(self, token):
-        #takes in receiving token and selects next state
-        # If a decision hasn't been made, we move to the next state
+        """Reads in and handles token sent by the other agent
+
+        If a decision hasn't been made, agent moves to next state defined
+        in the transition map.
+
+        Args:
+            token (int): token sent by opponent
+        """
+
         if (self.decision != NO_ACTION):
             self.set_state(self.transition_table[(self.state, token)])
     
     def reset(self):
+        """ Reset the agent's parameters """ 
+
         self.state = 0
         self.scores = []
         self.decision = NO_ACTION
 
     def get_state(self):
+        """ Return agent's automaton state """
         return self.state
         
     def set_state(self, state):
+        """ Set agent's state """
         self.state = state
-    
-    def step(self):
-        print(self.unique_id)
+
+
 
 class CommunicationModel(Model):
+    """ 
+    Represents agent-based model investigated in 'Communication and Cooperation' - Miller, et. al
+    """
     
-    def __init__(self, N, max_chat, fsm_size, num_tokens):      #fsm_size is number of states, num_tokens is complexity of language
-        
-        # TODO: Create agents
-        # TODO: take in params - max_chat, fsm_size, num_tokens
+    def __init__(self, N, max_chat_length, fsm_size, num_tokens):
+        """ Create a CC model with given parameters
+
+        Args:
+            N (int): number of agents in the model
+            max_chat_length (int): Maximum number of communications allowed
+            fsm_size (int): Number of states in agents' automata
+            num_tokens (int): Number of communication tokens allowed
+        """
         
         self.num_agents = N
-        self.max_chat = max_chat
+        self.max_chat_length = max_chat_length
         self.fsm_size = fsm_size
         self.num_tokens = num_tokens
         
@@ -91,141 +143,159 @@ class CommunicationModel(Model):
         self.single_gen_defections = 0
         self.total_chats = []
         self.single_gen_chats = []
-        self.total_proportions = []
-        self.single_gen_transition_mutations = 0
-        self.total_transition_muations = []
-        self.single_gen_action_mutations = 0
-        self.total_action_muations = []
+        self.total_proportions_cooperate = []
+        self.total_proportion_defect = []
         
         self.agents = []
         
         for i in range(self.num_agents):
             agent = CommunicationAgent(i, self, fsm_size, num_tokens)
             self.agents.append(agent)
-
-    def run_one_round(self, agent1, agent2):
-        pass
  
     def step(self):
         self.reset_agents()
-        self.selection()
+        self.run_generation()
     
-    def selection(self):
+    def run_generation(self):
+        """ Runs one generation of model
+
+        First, plays all agents against each other in single-shot PD game
+        Next, generates new population of same size as described in C+C
+        """
+ 
         self.single_gen_cooperations = 0
         self.single_gen_defections = 0
+        self.single_gen_no_actions = 0
         self.single_gen_chats = []
-        self.single_gen_transition_mutations = 0
-        self.single_gen_action_mutations = 0
 
+        # Generate all pairings of agents to play
         pairings = list(itertools.combinations(self.agents, 2))
+        num_games = len(pairings)
+
         for pair in pairings:
             self.play(pair[0], pair[1])
-        # pairings = Set
-        
-        #choose two agents to PLAY
-        # for agent1 in self.agents:
-        #     for agent2 in self.agents:
-        #         if agent1 != agent2:
-        #             self.play(agent1, agent2)
         
         self.total_cooperations.append(self.single_gen_cooperations)
         self.total_defections.append(self.single_gen_defections)
         self.total_chats.append(np.mean(self.single_gen_chats))
-        self.total_proportions.append(self.single_gen_cooperations / (self.single_gen_defections + self.single_gen_cooperations))
+        self.total_proportions_cooperate.append(self.single_gen_cooperations / num_games)
+        self.total_proportion_defect.append(self.single_gen_defections / num_games)
+
+        self.generate_new_population()
+
+    def generate_new_population(self): 
+        """ Generates a new population of the same size with random mutations
+
+        Runs a tournament-style selection with replacement to generate a new population of agents
+        Fitter of 2 agents selected has a 50% chance of mutation to either its action map or transition table
+        """
         
-        #better average scoring agent is placed into the new pool with a 50% probability that it will be mutated
-            #if it is mutated, 1) 50% chance that a random action_map state is changed (using the same 50/50 in gen_automata)
-                #or 2) 50% chance that a random transition_map state is changed
-        #RUN THIS SELECTION N TIMES for each agent; results in a new population of size N; run the step function again with this new population
+        logging.debug("GENERATING_POPULATION")
+        
         new_population = []
         for i in range(self.num_agents):
-            agent1 = random.choice(self.agents)
-            agent2 = random.choice(self.agents)
-            while(agent1 == agent2):
-                agent2 = random.choice(self.agents)
-            better_agent = agent1 if sum(agent1.scores) > sum(agent2.scores) else agent2
+
+            logging.debug("Begin-Selection {}".format(i))
+            agent1, agent2 = random.sample(self.agents, 2)
+            logging.debug("Agent1 ID:    {}   Agent2 ID:    {}".format(agent1.unique_id, agent2.unique_id))
+            logging.debug("Agent1 Score: {}   Agent2 Score: {}".format(np.mean(agent1.scores), np.mean(agent2.scores)))
+
+            better_agent = agent1 if np.mean(agent1.scores) > np.mean(agent2.scores) else agent2
             
-            if random.random() < .5: # roll for whether or not to mutate
-                if random.random() < .5: # roll for mutation type (change action map vs transition table)
-                    self.single_gen_action_mutations += 1
-                    if random.random() < .8: # roll for how to set new action map value (same as in agent's gen_automata function)
-                        better_agent.action_map[random.randrange(self.fsm_size)] = random.randint(1, self.num_tokens - 1)
+            logging.debug("Winning Agent: {}".format(better_agent.unique_id))
+
+
+            # Copy automata elements from better agent
+            new_action_map = copy.deepcopy(better_agent.action_map)
+            new_transition_table = copy.deepcopy(better_agent.transition_table)
+            
+            mutate_flip = random.random() < 0.5 # Roll for mutation
+            mutate_action = random.random() < 0.5 # Roll for mutation type - action map vs transition table
+
+            if mutate_flip: # roll for whether or not to mutate
+                if mutate_action: # roll for mutation type (change action map vs transition table)
+                    if random.random() < .5: # Roll for how to set new action map value
+                        new_action_map[random.randrange(self.fsm_size)] = random.randint(1, self.num_tokens - 1)
                     else: 
-                        better_agent.action_map[random.randrange(self.fsm_size)] = random.choice([COOPERATE, DEFECT])
+                        new_action_map[random.randrange(self.fsm_size)] = random.choice([COOPERATE, DEFECT])
                 else:
-                    self.single_gen_transition_mutations += 1
-                    better_agent.transition_table[random.choice(list(better_agent.transition_table.keys()))] = random.randrange(self.fsm_size)
-            new_agent = CommunicationAgent(better_agent.unique_id, self, self.fsm_size, self.num_tokens)
-            new_agent.transition_table = copy.copy(better_agent.transition_table)
-            new_agent.action_map = copy.copy(better_agent.action_map)
+                    new_transition_table[random.choice(list(new_transition_table.keys()))] = random.randrange(self.fsm_size)
+            
+            # Create new agent with better properties and set attributes
+            logging.debug('New Agent Transition Table: {}'.format(new_transition_table))
+            logging.debug('New Agent Action Map: {}'.format(new_action_map))
+            new_agent = CommunicationAgent(i, self, self.fsm_size, self.num_tokens)
+            new_agent.transition_table = new_transition_table
+            new_agent.action_map = new_action_map
+
             new_population.append(new_agent)
-            # new_population.append(copy.deepcopy(better_agent))
-        self.total_transition_muations.append(self.single_gen_transition_mutations)
-        self.total_action_muations.append(self.single_gen_action_mutations)
+            logging.debug("End-Selection {}\n".format(i))
+
+
         self.agents = new_population
+        
+        logging.debug("END_GENERATION\n")
 
     def play(self, agent1, agent2):
-        chat_count = 0
-        while chat_count < self.max_chat:
-            #still talking
+        """ Plays two agents against each other in a single-shot prisoner's dilemma
+
+        Args:
+            agent1 (CommunicationAgent): Agent 1 in the game
+            agent2 (CommunicationAgent): Agent 2 in the game
+        """
+
+        chat_length = 0
+        logging.debug('COMMUNICATION START')
+        while chat_length < self.max_chat_length:
+
+            # Choose actions for agent1 and agent2
             agent1_token = agent1.choose_action()
             agent2_token = agent2.choose_action()
             
+            # Both agents have decided - break
             if (agent1_token == 0 and agent2_token == 0):
                 break
             
+            # Agents handle tokens 
             agent1.handle_token(agent2_token)
             agent2.handle_token(agent1_token)
-            
-            chat_count += 1
-        self.single_gen_chats.append(chat_count)
+            logging.debug('Agent1 State: {} Token Sent: {}   |   Agent2 State: {}  Token Sent: {}'.format(agent1.state, agent1_token, agent2.state, agent2_token))
+            chat_length += 1
+        logging.debug('Chat length: {}'.format(chat_length))
+        logging.debug('END COMMUNICATION\n')
+        self.single_gen_chats.append(chat_length)
+
+        # Compute scores using payout dictionaryff
         agent1_score, agent2_score = payout[(agent1.decision, agent2.decision)]
-        if agent1.decision == COOPERATE and agent2.decision == COOPERATE:
-            self.single_gen_cooperations += 1
-        elif agent1.decision == DEFECT or agent2.decision == DEFECT:
-            self.single_gen_defections += 1
+
         agent1.scores.append(agent1_score)
         agent2.scores.append(agent2_score)
-        # return score of agents as a tuple (agent1_score, agent2_score)
+
+        # Record scores for statistics
+        if agent1.decision == COOPERATE and agent2.decision == COOPERATE:
+            self.single_gen_cooperations += 1
+        elif agent1.decision == DEFECT and agent2.decision == DEFECT:
+            self.single_gen_defections += 1
+        elif agent1.decision == NO_ACTION or agent2.decision == NO_ACTION:
+            self.single_gen_no_actions +=1
     
     def reset_agents(self):
         for agent in self.agents:
             agent.reset()
 
-
 if __name__ == '__main__':
-    communicationModel = CommunicationModel(50, 4, 4, 2)
-    for i in tqdm(range(5000)):
+    communicationModel = CommunicationModel(50, 10, 4, 4)
+    for i in tqdm(range(100)):
+        logging.debug('Generation Number {}'.format(i))
         communicationModel.step()
-    for agent in communicationModel.agents:
-        print(np.mean(agent.scores))
-    # print('Cooperations array')
-    # print(communicationModel.total_cooperations)
-    # plt.plot(communicationModel.total_cooperations)
-    # plt.title('Cooperations')
-    # plt.show()
-    # print('Defections array')
-    # print(communicationModel.total_defections)
-    # plt.plot(communicationModel.total_defections)
-    # plt.title('Defections')
-    # plt.show()
 
-    print('Proportions array')
-    print(communicationModel.total_proportions)
-    plt.plot(communicationModel.total_proportions)
-    plt.title('Proportion of mutual cooperations')
-    plt.show()
+    # Plot proportion of mutual cooperative games
+    plt.subplot(2, 1, 1)
+    plt.plot(communicationModel.total_proportions_cooperate)
+    plt.title('Proportion of cooperate / defect')
 
-    print('Communications array')
-    print(communicationModel.total_chats)
+    # Plot average chat length for each game
+    plt.subplot(2, 1, 2)
     plt.plot(communicationModel.total_chats)
     plt.title('Chats')
-    plt.show()
-    
-    plt.plot(communicationModel.total_action_muations)
-    plt.title('Action map mutations')
-    plt.show()
-    
-    plt.plot(communicationModel.total_transition_muations)
-    plt.title('Transition table mutations')
     plt.show()
